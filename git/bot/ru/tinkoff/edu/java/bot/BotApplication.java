30d928e241292c25324061d424ee9daa3135fbe8
sleepyghostie <paradoxizz@mail.ru>
2023-03-26 22:54:56 +0300

    link parser classes

--- git/bot/ru/tinkoff/edu/java/bot/BotApplication.java
+++
@@ -1,9 +0,0 @@
-@SpringBootApplication
-@EnableConfigurationProperties(ApplicationConfig.class)
-public class BotApplication {
-  public static void main(String[] args) {
-      var ctx = SpringApplication.run(ScrapperApplication.class, args);
-      ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
-      System.out.println(config);
-  }
-}
