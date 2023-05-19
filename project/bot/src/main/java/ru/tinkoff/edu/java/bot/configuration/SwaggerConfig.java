b36c3467a081f6bf611f42f4216a21dac619800d
sleepyghostie <paradoxizz@mail.ru>
2023-05-19 19:19:02 +0300

    fix

--- project/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/SwaggerConfig.java
+++ project/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
                                 "<a href=\"https://gist.github.com/sanyarnd/52a78a01fa9ec234c8ad50fbc5ecc9e4\" target=\"_blank\">specification</a>")
                         .version("1.0.0")
                         .contact(new Contact()
-                                .name("Saifutdinova Tatyana")
+                                .name("SaifutdinovaTatyana")
                                 .email("paradoxizz@mail.ru")));
     }
 }
